6b11d320aecedb0f92577fd281a5e218e7922895
LordBenzo <50697087+lorenzodelmedico@users.noreply.github.com>
2020-11-02 23:04:51 +0100

    Update Numberofpath.java

--- Numberofpath.java
+++ Numberofpath.java
@@ -6,7 +6,7 @@ import java.math.*;
  * Auto-generated code below aims at helping you parse
  * the standard input according to the problem statement.
  **/
-class Solution {
+class Numberofpath {
 
 
     //Configuration of the Matrix 
