a86b7559488a757c505e052374cab544c6c039bb
Lorenzo DelMedico <lorenzodelmedico@gmail.com>
2021-01-04 14:18:28 +0100

    [bronze I] code with comments

--- DM_TronGame/Player.java
+++ DM_TronGame/Player.java
@@ -10,19 +10,33 @@ import java.math.*;
 //Enregistre la position de chaque joueur 
     //Construire un graphe avec la position actualisée de chaque joueur/tour 
 
-    //debut : se diriger sur la grid en tenant compte de la position des adversaires et si le chemin a deja été visité ou non 
+    //strategie : se diriger sur la grid en tenant compte de la position des adversaires et si le chemin a deja été visité ou non 
 
-
+    // ranking [bronze I] : 1149 
+    //Idées pour améliorer : 
 
 class Player {
 
     
-    public static String fillPlayerPosition(int X0, int Y0){
+    /* Usage : Sert à remplir un 2d array avec les inputs des positions des players : 
+    {
+        Args : int X, int Y représente la position X et Y du joueur sur le plateau
+        Return : un String sous la forme "X,Y" qui va remplir le String 2dArray positionPlayers
+    }
+    */
+    public static String fillPlayerPosition(int X, int Y){
         
-        String tmpArray = X0 + "," + Y0;
+        String tmpArray = X + "," + Y;
         return tmpArray;
     }
 
+    /* Usage :sert à gérer le déplacement de notre player : 
+    {
+        Args : String[][] playersPosition : 2dArray qui comprend les positions occupées ; int currentX, int currentY : représente la position actuelle de notre player ; int compteur : représente le nombre de tour joué
+        Body : On appel 2 fonctions mooveTo et mooveToBorder qui gère les déplacements et ses restrictions
+        Return : un String sous la forme "UP" || "DOWN" || "LEFT" || "RIGHT" qui va indiquer la prochaine direction à suivre 
+    }
+    */
     public static String mooveManager(String[][] playersPosition, int currentX, int currentY, int compteur){
         if (currentY == 0 || currentY == 20){
             return mooveTo(playersPosition, currentX, currentY, compteur);
@@ -33,8 +47,13 @@ class Player {
     }
         
        
-
-
+    /* Usage :sert à forcer le déplacement de notre player vers une border distante : 
+    {
+        Args : String[][] playersPosition : 2dArray qui comprend les positions occupées ; int currentX, int currentY : représente la position actuelle de notre player ; int compteur : représente le nombre de tour joué
+        Body : On essaye de forcer le déplacement du player vers la border dont il se trouve le plus éloigné. 
+        Return : un String sous la forme "UP" || "DOWN" || "LEFT" || "RIGHT" qui va indiquer la prochaine direction à suivre , si aucune situation ne correspond on fait appel à la fonction mooveTo qui propose une autre gestion
+    }
+    */
     public static String mooveToBorder(String[][] playersPosition, int currentX, int currentY, int compteur){
         if (20 - currentY > 10){
             String positionTest = currentX + "," + (currentY+1);
@@ -64,6 +83,12 @@ class Player {
     }
 
 
+    /* Usage : Methode qui test les positions adjacentes à la notre pour voir si on peut s'y déplacer : 
+    {
+        Args :  String[][] playersPosition : 2dArray qui comprend les positions occupées ; int currentX, int currentY : représente la position actuelle de notre player ; int compteur : représente le nombre de tour joué
+        Body : Test une par une les positions adjacentes pour trouver celle vers laquelle on peut se déplacer, en favorisant sur l'axe Y par rapport à l'axe X. 
+        Return : un String qui vaut soit "UP" || "DOWN" || "RIGHT" || "LEFT" ; 
+    */
     public static String mooveTo(String[][] playersPosition, int currentX, int currentY, int compteur){
         String positionTest = currentX + "," + (currentY+1);
         if (canMoove(playersPosition, positionTest, compteur )){
@@ -85,7 +110,15 @@ class Player {
         return mooveDirector(currentX, currentY, currentX, currentY);
     }
 
+    /* Usage : Methode qui gère la direction à prendre, en fonction de la position actuelle : 
+    {
+        Args : int currentX, int currentY : représente la position actuelle ; int nextX, int nextY : représente la case sur laquelle on veut se rendre. 
+        Return : un String qui vaut soit "UP" || "DOWN" || "RIGHT" || "LEFT" ; Si aucune situation ne correspond, cela signifie probablement que j'ai perdu, 
+        je décide donc de renvoyer une direction afin de ne pas perdre par réponse non fournie. 
+    */
+
     public static String mooveDirector(int currentX, int currentY, int nextX, int nextY){
+        
         if ((currentY-1) == nextY){
             return "UP";
         }
@@ -97,9 +130,15 @@ class Player {
         }if ((currentX-1) == nextX){
             return "LEFT";
         }
-
-        return "LEFT";
+        return "DOWN";
     }
+
+    /* Usage : Methode qui englobe des checkers qui regarde si la position à atteindre est accessible : 
+    {
+        Args : String[] positionArray : array des positions d'un joueur ; String position : la position sur laquelle on souhaite se déplacér ; int compteur : le nombre de tours joué
+        Return : un boolean qui vaut true si la case est disponible, false sinon. 
+    }
+    */
 
     public static boolean canMoove(String[][] playersPosition, String position, int compteur){
         for (int i=0; i<playersPosition.length; i++){
@@ -107,12 +146,19 @@ class Player {
                 return false;
             }
             if (isBorder(position)){
+               
                 return false;
             }
         }
         return true;
     }
 
+     /* Usage : Methode qui regarde si une case est déjà occupé : 
+    {
+        Args : String[] positionArray : array des positions d'un joueur ; String position : la position sur laquelle on souhaite se déplacér ; int compteur : le nombre de tours joué
+        Return : un boolean qui vaut true si la case est occupé, false sinon. 
+    }
+    */
     public static boolean isOccupied(String[] positionArray, String position, int compteur){
         for (int i=0; i<compteur+1; i++){
             if (positionArray[i].equals(position)){
@@ -122,8 +168,15 @@ class Player {
         return false;
     }
 
+     /* Usage : Methode qui regarde si une case est hors-limite : 
+    {
+        Args : String position : la position sur laquelle on souhaite se déplacér 
+        Return : un boolean qui vaut true si la case est hors-limite, false sinon. 
+    }
+    */
+
     public static boolean isBorder(String position){
-        if (position.startsWith("0,")){
+        if (position.startsWith("-1,")){
             return true;
         }
         if (position.contains("30,")){
@@ -132,7 +185,7 @@ class Player {
         if (position.contains(",20")){
             return true;
         }
-        if (position.contains(",0")){
+        if (position.contains(",-1")){
             return true;
         }
         return false;
